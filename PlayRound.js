a07f8443d65850c5c8be90f84ee127c32170a62e
Gabriel Retamales <gabriel@pop-os.localdomain>
2022-06-26 16:41:48 -0400

    Working game

--- PlayRound.js
+++ PlayRound.js
@@ -20,15 +20,15 @@ function playRound(humplay, complay) {
     ElementalArray = ['Fire', 'Water', 'Plant']
     let comElement = ElementalArray[complay];
 
-    if (caseInsensitive(humplay) === comElement) { return ('Its a Tie')} 
-    else if (caseInsensitive(humplay) === 'Plant' && comElement === 'Water') {return ('You Win ! '+caseInsensitive(humplay)+' beats '+comElement+'.')}
-    else if (caseInsensitive(humplay) === 'Plant' && comElement === 'Fire') {return ('You Lost ! '+comElement+' beats '+caseInsensitive(humplay)+'.')}
+        if (caseInsensitive(humplay) === comElement) { return ('Its a Tie')} 
+        else if (caseInsensitive(humplay) === 'Plant' && comElement === 'Water') {return ('You Win ! '+caseInsensitive(humplay)+' beats '+comElement+'.')}
+        else if (caseInsensitive(humplay) === 'Plant' && comElement === 'Fire') {return ('You Lost ! '+comElement+' beats '+caseInsensitive(humplay)+'.')}
 
-    else if (caseInsensitive(humplay) === 'Fire' && comElement === 'Water') {return ('You Lost ! '+caseInsensitive(humplay)+' beats '+comElement+'.')}
-    else if (caseInsensitive(humplay) === 'Fire' && comElement === 'Plant') {return ('You Win ! '+caseInsensitive(humplay)+' beats '+comElement+'.')}
+        else if (caseInsensitive(humplay) === 'Fire' && comElement === 'Water') {return ('You Lost ! '+caseInsensitive(humplay)+' beats '+comElement+'.')}
+        else if (caseInsensitive(humplay) === 'Fire' && comElement === 'Plant') {return ('You Win ! '+caseInsensitive(humplay)+' beats '+comElement+'.')}
 
-    else if (caseInsensitive(humplay) === 'Water' && comElement === 'Fire') {return ('You Win ! '+caseInsensitive(humplay)+' beats '+comElement+'.')}
-    else if (caseInsensitive(humplay) === 'Water' && comElement === 'Plant') {return ('You Lost ! '+caseInsensitive(humplay)+' beats '+comElement+'.')}
+        else if (caseInsensitive(humplay) === 'Water' && comElement === 'Fire') {return ('You Win ! '+caseInsensitive(humplay)+' beats '+comElement+'.')}
+        else if (caseInsensitive(humplay) === 'Water' && comElement === 'Plant') {return ('You Lost ! '+caseInsensitive(humplay)+' beats '+comElement+'.')}
   }
   
   //Testing variables
@@ -42,22 +42,27 @@ function game(){
 
     for (let i = 0; i < 5; i++) {
         let playerSelection = prompt('Select your Element', 'Fire, Water or Plant')
-        let result = playRound(playerSelection, computerSelection())
+        if (caseInsensitive(playerSelection) !== 'Fire' && caseInsensitive(playerSelection) !== 'Water' && caseInsensitive(playerSelection) !== 'Plant') {return alert('Please choose an elemental listed before: Fire, Water, Plant') 
+        i = 0
+        continue}    
+        else {
+            let result = playRound(playerSelection, computerSelection())
 
-        if(result.charAt(4) === 'W'){humCount++}
-        else if (result.charAt(4) === 'L'){comCount++}
+            if(result.charAt(4) === 'W'){humCount++}
+            else if (result.charAt(4) === 'L'){comCount++}
 
-        console.log(result)
-        console.log('Scores are You: '+humCount+' | Computer: '+comCount+'.')
+            console.log(result)
+            console.log('Scores are You: '+humCount+' | Computer: '+comCount+'.')
 
-        if (i <= 5 && (humCount !== 5 && comCount !== 5)) {
-            i--}
-        else if(humCount === 5) 
-            {console.log('You won the Elemental Fight !')
-            i = 5}
-        else if (comCount === 5) 
-            {console.log('You Lost boooo!')
-            i = 5}
+            if (i <= 5 && (humCount !== 5 && comCount !== 5)) {
+                i--}
+            else if(humCount === 5) 
+                {console.log('You won the Elemental Fight !')
+                i = 5}
+            else if (comCount === 5) 
+                {console.log('You Lost boooo!')
+                i = 5}
+            }
      }
   }
 
